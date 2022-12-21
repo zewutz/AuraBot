f9eb296151c183bdb4936fb47526bf894a1108ce
Bîrsan Silviu-Ionuț <55995994+zewutz@users.noreply.github.com>
2022-12-21 18:02:54 +0200

    Delete database directory

--- database/table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (username text,
-                    usertag text,
-                    userid text);
-
-CREATE TABLE guilds (
-            id text,
-            prefix text
-);
-
